6fc90efe979c89b771a6a6bad826721a6e410e85
dilorixGH <dsdilarix@gmail.com>
2026-08-25 10:09:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -161,34 +161,35 @@ function renderCalendar() {
 
     const year = currentDate.getFullYear();
     const month = currentDate.getMonth();
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
 
     calendarMonth.textContent = currentDate.toLocaleString("ru", {
         month: "long",
         year: "numeric"
     });
 
-    const firstDay = new Date(year, month, 1).getDay();
-    const daysInMonth = new Date(year, month + 1, 0).getDate();
-
-    for (let i = 0; i < (firstDay === 0 ? 6 : firstDay - 1); i++) {
-        const empty = document.createElement("div");
-        calendarGrid.appendChild(empty);
-    }
-
-    for (let day = 1; day <= daysInMonth; day++) {
+    // Всегда 42 ячейки
+    for (let i = 0; i < 42; i++) {
         const cell = document.createElement("div");
-        cell.className = "calendar-day";
-        cell.textContent = day;
 
-        cell.onclick = () => {
-            const d = new Date(year, month, day);
-            inputDate.value = d.toISOString().split("T")[0];
-            closeCalendar();
-        };
+        if (i < daysInMonth) {
+            const day = i + 1;
+            cell.className = "calendar-day";
+            cell.textContent = day;
+
+            cell.onclick = () => {
+                const d = new Date(year, month, day);
+                inputDate.value = d.toISOString().split("T")[0];
+                closeCalendar();
+            };
+        } else {
+            cell.className = "calendar-day empty-day";
+        }
 
         calendarGrid.appendChild(cell);
     }
 }
+
 
 prevMonthBtn.onclick = () => {
     currentDate.setMonth(currentDate.getMonth() - 1);
